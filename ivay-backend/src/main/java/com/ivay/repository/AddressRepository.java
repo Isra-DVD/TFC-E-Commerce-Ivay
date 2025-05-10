0a9f0e9abe8475e1eb0b2fb1a238b56824585b08
Isra-DVD <159429781+Isra-DVD@users.noreply.github.com>
2025-05-11 00:32:27 +0100

    Revert "Refact frontend product details"

--- ivay-backend/src/main/java/com/ivay/repository/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ivay.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.ivay.entity.Address;
-
-@Repository
-public interface AddressRepository extends JpaRepository<Address, Long> {
-}
