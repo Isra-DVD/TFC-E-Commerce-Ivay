47e6e341dd0f2fc00dfaa56a2a68af4a9fb45508
Isra-DVD <usr3849@salesianos-lacuesta.net>
2025-05-16 20:45:14 +0100

    feat(): Implemented zipCode, province and locality fields to Address entity and updated dtos, service and controller

--- ivay-backend/src/main/java/com/ivay/dtos/addressdto/AddressRequestDto.java
+++ ivay-backend/src/main/java/com/ivay/dtos/addressdto/AddressRequestDto.java
@@ -16,4 +16,16 @@ public class AddressRequestDto {
 	@NotBlank(message = "La dirección es obligatoria")
 	@Size(max = 255, message = "La dirección no puede exceder 255 caracteres")
     private String address;
+	
+	@NotBlank(message = "El código postal es obligatorio")
+    @Size(max = 7, message = "El código postal no puede exceder 7 caracteres")
+    private String zipCode;
+	
+	@NotBlank(message = "La provincia es obligatoria")
+	@Size(max = 50, message = "La provincia no puede exceder 50 caracteres")
+    private String province;
+	
+	@NotBlank(message = "La localidad es obligatoria")
+	@Size(max = 50, message = "La localidad no puede exceder 50 caracteres")
+    private String locality;
 }
